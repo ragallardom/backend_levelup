4461f9e741eeadf9acb153ef60dcafff6fe152a1
ragallardom <ragallardom@outlook.com>
2025-11-08 23:22:18 -0300

    first commit

---
+++ src/main/java/com/levelup/backend_levelup/BackendLevelupApplication.java
@@ -0,0 +1,13 @@
+package com.levelup.backend_levelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLevelupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLevelupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/levelup/backend_levelup/BackendLevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.backend_levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLevelupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
